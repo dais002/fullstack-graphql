2b51c71b7f5d55cd21ba82176d5fc45db0c9760d
Stanford <stanfordhdai@gmail.com>
2020-03-24 10:10:13 -0700

    first commit on intro to graphql

--- api/src/schema.js
+++ api/src/schema.js
@@ -4,7 +4,17 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
 
 `;
 
